9ccf9ad5b41057d214ef12758e218275a6eca307
Ed-Silverio-Mr-Booleano <edmilsondaibinho0@gmail.com>
2022-07-12 15:28:32 +0100

    feat: should retun 200 if data was updated

--- src/presentation/routers/task-router.ts
+++ src/presentation/routers/task-router.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from 'express'
 import { GetAllTasksUseCase } from '../../domain/interfaces/use-cases/get-all-task-use-case'
 import { CreateTaskUseCase } from '../../domain/interfaces/use-cases/create-task-use-case'
 import { DeleteTaskUseCase } from '../../domain/interfaces/use-cases/delete-task-use-case'
+import { UpdateTaskUseCase } from '../../domain/interfaces/use-cases/update-task-use-case'
 
 export default function TasksRouter (
   getAllTasksUseCase: GetAllTasksUseCase,
   createTaskUseCase: CreateTaskUseCase,
-  deleteTaskUseCase: DeleteTaskUseCase
+  deleteTaskUseCase: DeleteTaskUseCase,
+  updateTaskUseCase: UpdateTaskUseCase
 ): express.Router {
   const router = express.Router()
 
@@ -35,10 +37,23 @@ export default function TasksRouter (
     try {
       await deleteTaskUseCase.execute(request.params.id)
       response.statusCode = 200
+      console.log('param id: ', request.params.id)
       response.json({ message: 'Deleted' })
     } catch (error) {
       console.log(error.message)
-      response.status(500).send({ message: 'Error saving data' })
+      response.status(500).send({ message: 'Error deleting data' })
+    }
+  })
+
+  router.put('/:id', async (request: Request, response: Response) => {
+    try {
+      await updateTaskUseCase.execute(request.params.id, request.body)
+      response.statusCode = 200
+      console.log(`param id: ${request.params.id}`)
+      response.json({ message: 'Updated' })
+    } catch (error) {
+      console.log(error.message)
+      response.status(500).send({ message: 'Error updating data' })
     }
   })
 
