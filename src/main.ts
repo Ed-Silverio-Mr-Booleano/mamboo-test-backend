90cc3eab896d93ec2a340de5bec3d145a4b6e10a
Ed-Silverio-Mr-Booleano <edmilsondaibinho0@gmail.com>
2022-07-12 15:50:33 +0100

    refactor: ensure main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { GetAllTasks } from './domain/use-cases/task/get-all-task'
 import { TaskRepositoryImpl } from './domain/repositories/task-repository'
 import { DeleteTask } from './domain/use-cases/task/delete-task'
 import { CreateTask } from './domain/use-cases/task/create-task'
+import { UpdateTask } from './domain/use-cases/task/update-task'
 import { MongoClient } from 'mongodb'
 import { NoSQLDatabaseWrapper } from './data/interfaces/data-source/nosql-database-wrapper'
 import { MongoDBTaskDataSource } from './data/data-sources/mongodb/mongodb-contact-data-source'
@@ -22,7 +23,7 @@ async function getMongoDS (): Promise<MongoDBTaskDataSource> {
     find: (query) => db.collection(process.env.COLLECTION).find(query).toArray(),
     insertOne: (doc) => db.collection(process.env.COLLECTION).insertOne(doc),
     deleteOne: (id: String) => db.collection(process.env.COLLECTION).deleteOne({ _id: id }),
-    updateOne: (id: String, data: object) => db.collection(process.env.COLLECTION).updateOne({ _id: id }, data)
+    updateOne: (id: String, data: object) => db.collection(process.env.COLLECTION).replaceOne({ _id: id }, data)
   }
 
   return new MongoDBTaskDataSource(taskDatabase)
@@ -33,7 +34,8 @@ async function getMongoDS (): Promise<MongoDBTaskDataSource> {
   const taskMiddleware = TasksRouter(
     new GetAllTasks(new TaskRepositoryImpl(dataSource)),
     new CreateTask(new TaskRepositoryImpl(dataSource)),
-    new DeleteTask(new TaskRepositoryImpl(dataSource))
+    new DeleteTask(new TaskRepositoryImpl(dataSource)),
+    new UpdateTask(new TaskRepositoryImpl(dataSource))
   )
   server.use('/tasks', taskMiddleware)
   server.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`))
