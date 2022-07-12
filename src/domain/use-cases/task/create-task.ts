89f3d93697cedf64262cf9c990427abb4f2c27a2
Ed-Silverio-Mr-Booleano <edmilsondaibinho0@gmail.com>
2022-07-12 07:12:48 +0100

    feat: add create-task use-case

---
+++ src/domain/use-cases/task/create-task.ts
@@ -0,0 +1,14 @@
+import { TaskRequestEntity } from '../../entities/task'
+import { TaskRepository } from '../../interfaces/repositories/task-repository'
+import { CreateTaskUseCase } from '../../interfaces/use-cases/create-task-use-case'
+
+export class CreateContact implements CreateTaskUseCase {
+  TaskRepository: TaskRepository
+  constructor (TaskRepository: TaskRepository) {
+    this.TaskRepository = TaskRepository
+  }
+
+  async execute (task: TaskRequestEntity): Promise<void> {
+    await this.TaskRepository.createTask(task)
+  }
+}
